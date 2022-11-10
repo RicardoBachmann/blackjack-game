f94434d538caa742f661da1b32eeaa502b86f507
RicardoBachmann <ricardo.bachmann@icloud.com>
2022-11-10 13:14:35 +0100

    Add frist if/else conditionals

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 let firstCard = 5
-let secondCard = 10
+let secondCard = 3
 
 let sum = firstCard + secondCard
 
-console.log(sum);
+if (sum < 21) {
+    console.log("Do you want to draw a new card?") 
+} else if (sum === 21) {
+    console.log("You have got Blackjack")
+} else {
+    console.log("You are out of the game")
+}
