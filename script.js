3da61ee932c090c89dc1682e789214af9daf5bca
RicardoBachmann <ricardo.bachmann@icloud.com>
2022-11-10 13:44:42 +0100

    Add the isAlive variable

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 let firstCard = 5
-let secondCard = 3
-let sum = firstCard + secondCard + 15
+let secondCard = 20
+let sum = firstCard + secondCard 
 let hasBlackJack = false
+let isAlive = true
 
 if (sum <= 21) {
     console.log("Do you want to draw a new card?") 
 } else if (sum === 21) {
     console.log("You have got Blackjack")
-    hasBlackJack = true
+    hasBlackJack = true;
 } else {
-    console.log("You are out of the game")
+    isAlive = false;
+    console.log("You are out of the game!")
 } 
 
-console.log(hasBlackJack);
+console.log(isAlive)
+
