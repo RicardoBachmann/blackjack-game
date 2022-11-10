9c9611aace342f5a2ef93a4263d0322cc3ecbb57
RicardoBachmann <ricardo.bachmann@icloud.com>
2022-11-10 15:40:29 +0100

    Add basic styling

--- script.js
+++ script.js
@@ -5,15 +5,20 @@ let hasBlackJack = false
 let isAlive = true
 let message = ""
 
-if (sum <= 21) {
-    message = "Do you want to draw a new card?" 
-} else if (sum === 21) {
-    message = "You've got Blackjack!"
-    hasBlackJack = true
-} else {
-    message = "You're out of the game."
-    isAlive = false
-} 
+function startGame() {
+    
+    if (sum <= 21) {
+        message = "Do you want to draw a new card?" 
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game."
+        isAlive = false
+    } 
+  
+}
 
-console.log(message)
+
+
 
