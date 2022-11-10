b10f9cd57ab823acaa9398f65c196c76c240c670
RicardoBachmann <ricardo.bachmann@icloud.com>
2022-11-10 16:13:10 +0100

    Display message, sum and cards

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-let firstCard = 5
-let secondCard = 3
+let firstCard = 10
+let secondCard = 11
 let sum = firstCard + secondCard 
 let hasBlackJack = false
 let isAlive = true
 let message = ""
 
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+
 function startGame() {
     
-    if (sum <= 21) {
+    if (sum <= 20) {
         message = "Do you want to draw a new card?" 
     } else if (sum === 21) {
         message = "You've got Blackjack!"
@@ -17,6 +21,10 @@ function startGame() {
         isAlive = false
     } 
   
+   messageEl.textContent = message
+   sumEl.textContent = "Sum: " + sum
+   cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    
 }
 
 
