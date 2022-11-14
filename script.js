cfff90fecd70ba053ab0cf93e106e2f3f450874d
Ricardo Bachmann <ricardo.bachmann@icloud.com>
2022-11-14 14:16:30 +0100

    For loop to render cards

--- script.js
+++ script.js
@@ -16,7 +16,11 @@ function startGame() {
 
 function renderGame() {
     
-    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+
     sumEl.textContent = "Sum: " + sum
     
     if (sum <= 20) {
